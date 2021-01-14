18835e3f157d4e5653e35070704b817889fc6adf
BostonSlettum22 <816661@mystma.org>
2021-01-14 16:39:56 +0000

    calc 1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#WRITE YOUR CODE IN THIS FILE
+def calculate(z,x,y):
+    if z == "add":
+        return x + y
+    elif z == "subtract":
+        return x - y
+    elif z == "multiply":
+        return x * y
+    elif z == "divide":
+        return x / y
+    else:
+        return 0
+        
+
+print(calculate("divide", 20, 5))
